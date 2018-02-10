aeb293f72a8cfc8911ad5b26f19d65c0f44b40bf
Janie <emerald1901@gmail.com>
2018-02-10 15:19:57 +0700

    First commit

--- blockchain.go
+++ blockchain.go
@@ -3,8 +3,10 @@ package main
 import (
 	"bytes"
 	"crypto/sha256"
+	"errors"
 	"fmt"
 	"strconv"
+	"time"
 
 	"github.com/davecgh/go-spew/spew"
 )
@@ -25,12 +27,32 @@ func InitBlockchain() {
 	fmt.Println("******TODO: IMPLEMENT InitBlockchain!******")
 	spew.Dump(Blockchain)
 	// Fill me in, noble warrior.
+
+	genesis := Block{
+		"data",
+		time.Now().Unix(),
+		[]byte{},
+		[]byte{},
+	}
+
+	genesis.Hash = genesis.calculateHash()
+
+	Blockchain = append(Blockchain, genesis)
 }
 
 // NewBlock creates a new Blockchain Block.
 func NewBlock(oldBlock Block, data string) Block {
 	fmt.Println("******TODO: IMPLEMENT NewBlock!******")
-	return Block{}
+	newBlock := Block{
+		Data:      data,
+		Timestamp: time.Now().Unix(),
+		PrevHash:  oldBlock.Hash,
+		Hash:      []byte{},
+	}
+
+	newBlock.Hash = newBlock.calculateHash()
+
+	return newBlock
 }
 
 // AddBlock adds a new block to the Blockchain.
@@ -38,6 +60,12 @@ func AddBlock(b Block) error {
 	fmt.Println("******TODO: IMPLEMENT AddBlock!******")
 	spew.Dump(Blockchain)
 	// Fill me in, brave wizard.
+	if b.verifyHash(Blockchain[len(Blockchain)-1]) {
+		Blockchain = append(Blockchain, b)
+	} else {
+		return errors.New("Invalid block")
+	}
+
 	return nil
 }
 
@@ -47,4 +75,9 @@ func (b *Block) calculateHash() []byte {
 	headers := bytes.Join([][]byte{b.PrevHash, data, timestamp}, []byte{})
 	hash := sha256.Sum256(headers)
 	return hash[:]
+}
+
+func (b *Block) verifyHash(prevBlock Block) bool {
+	prevHash := prevBlock.calculateHash()
+	return bytes.Equal(prevHash, b.PrevHash)
 }
